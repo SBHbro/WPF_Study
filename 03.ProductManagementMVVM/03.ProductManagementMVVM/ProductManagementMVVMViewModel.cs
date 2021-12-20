823428084f668bb018f118710aea93209094ceb1
SBHbro <ssd018@gmail.com>
2021-12-20 16:43:44 +0900

    Add 03.ProductManagementMVVM

---
+++ 03.ProductManagementMVVM/03.ProductManagementMVVM/ProductManagementMVVMViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03.ProductManagementMVVM
+{
+    class ProductManagementMVVMViewModel : Notifier
+    {
+        private ProductsFactory productsFactory;
+
+        private string searchText;
+        public string SearchText
+        {
+            get { return searchText; }
+            set
+            {
+                searchText = value;
+                OnPropertyChanged("SearchText");
+                OnSearchTextChanged();
+            }
+        }
+
+        private IEnumerable<Product> productsList;
+        public IEnumerable<Product> ProductsList
+        {
+            get { return productsList; }
+            set
+            {
+                productsList = value;
+                OnPropertyChanged("ProductsList");
+            }
+        }
+
+        public ProductManagementMVVMViewModel()
+        {
+            productsFactory = new ProductsFactory();
+            ProductsList = productsFactory.GetAllProducts();
+        }
+
+        private void OnSearchTextChanged()
+        {
+            ProductsList = productsFactory.FindProducts(SearchText);
+        }
+    }
+}
