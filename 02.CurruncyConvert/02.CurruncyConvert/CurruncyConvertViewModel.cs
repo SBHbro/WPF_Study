089d2314bf017a38123dfd51d93e1c6ea3757eee
SBHbro <ssd018@gmail.com>
2021-12-20 13:35:18 +0900

    Add 02.CurruncyConvert

---
+++ 02.CurruncyConvert/02.CurruncyConvert/CurruncyConvertViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _02.CurruncyConvert
+{
+    class CurruncyConvertViewModel : Notifier
+    {
+        private decimal won;
+        public decimal Won
+        {
+            get { return won; }
+            set
+            {
+                won = value;
+                OnPropertyChanged("Won");
+                OnWonChanged();
+            }
+        }
+
+        private decimal dollor;
+        public decimal Dollor
+        {
+            get { return Decimal.Round(dollor,2); }
+            set
+            {
+                dollor = value;
+                OnPropertyChanged("Dollor");
+            }
+        }
+
+        private void OnWonChanged()
+        {
+            Dollor = Won / 1200;
+        }
+    }
+}
